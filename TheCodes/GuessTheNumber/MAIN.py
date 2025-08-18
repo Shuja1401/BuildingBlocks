3dc396e71258b6dd0806f583c4704fe020c7c0e5
Shuja1401 <asrar.shuja@gmail.com>
2025-08-18 22:44:42 +0530

    Create MAIN.py
    This program asks the user to guess a randomly generated number.

---
+++ TheCodes/GuessTheNumber/MAIN.py
@@ -0,0 +1,35 @@
+Guess the number:
+ The number should be between 1 and 1,000
+The number should be an integer
+200
+You value is smaller
+Guess the number:
+ The number should be between 1 and 1,000
+The number should be an integer
+900
+You value is smaller
+Guess the number:
+ The number should be between 1 and 1,000
+The number should be an integer
+950
+Your value is bigger.
+Guess the number:
+ The number should be between 1 and 1,000
+The number should be an integer
+930
+You value is smaller
+Guess the number:
+ The number should be between 1 and 1,000
+The number should be an integer
+940
+Your value is bigger.
+Guess the number:
+ The number should be between 1 and 1,000
+The number should be an integer
+933
+You value is smaller
+Guess the number:
+ The number should be between 1 and 1,000
+The number should be an integer
+936
+Congratulations! You guessed the correct number
